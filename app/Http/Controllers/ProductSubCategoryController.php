c4416d32c360d2a4d52116c3efa2c333ef8b4ab7
yayanrw <yayanraw@gmail.com>
2024-01-02 16:15:49 +0700

    create 8 files

---
+++ app/Http/Controllers/ProductSubCategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductSubCategory;
+use App\Http\Requests\StoreProductSubCategoryRequest;
+use App\Http\Requests\UpdateProductSubCategoryRequest;
+
+class ProductSubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProductSubCategoryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProductSubCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductSubCategory  $productSubCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductSubCategory  $productSubCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProductSubCategoryRequest  $request
+     * @param  \App\Models\ProductSubCategory  $productSubCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProductSubCategoryRequest $request, ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductSubCategory  $productSubCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_01_02_090846_create_product_sub_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('sub_category');
+            $table->boolean('is_active')->default(true);
+            $table->string('created_by');
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_sub_categories');
+    }
+};
