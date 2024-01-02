130b82ae9a3e9cbaee833a246ff85c284ab8446b
yayanrw <yayanraw@gmail.com>
2024-01-02 16:47:27 +0700

    create 8 files

---
+++ database/migrations/2024_01_02_094336_create_abouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('img_url')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('whatsapp')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('lat', 10, 8)->nullable();
+            $table->decimal('lon', 11, 8)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
