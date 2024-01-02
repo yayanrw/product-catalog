5851662b35faf3d6197c2fcfcaeefd59a8d023cf
yayanrw <yayanraw@gmail.com>
2024-01-02 15:09:54 +0700

    create 8 files

---
+++ database/migrations/2024_01_02_080855_create_brands_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand');
+            $table->string('img_url');
+            $table->boolean('is_active')->default(true);
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+};
