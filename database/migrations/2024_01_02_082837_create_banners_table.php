7b4632e142894bfe7e1b79ecc15bae4cf6a1a1dc
yayanrw <yayanraw@gmail.com>
2024-01-02 15:30:56 +0700

    create 8 files

---
+++ database/migrations/2024_01_02_082837_create_banners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('sub_title');
+            $table->text('description');
+            $table->string('img_url');
+            $table->boolean('is_active')->default(true);
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+};
